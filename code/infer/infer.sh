3075c8f22d066f03bd82e5f2f1f396a104c0a830
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2019-04-04 14:26:49 -0400

    writing oopsla overview

--- code/infer/infer.sh
+++ code/infer/infer.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-lein typed infer-type infer.core :infer-opts "{:out-dir \"out\"}"
+lein typed infer-type infer.core :infer-opts "{:fuel 0 :debug :all :out-dir \"out\" :preserve-unknown true}"
