ec166380bbd617842e78e20c24d90d19dd174a67
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2019-04-05 10:04:46 -0400

    overview

--- code/infer/test/infer/visit_leaf_test.clj
+++ code/infer/test/infer/visit_leaf_test.clj
@@ -1,5 +1,6 @@
 (ns infer.visit-leaf-test
   (:require [clojure.test :refer :all]
+            [clojure.walk :as w]
             [infer.visit-leaf :refer :all]))
 
 (def t1 {:op :node, :left {:op :leaf, :val 1}, :right {:op :leaf, :val 2}})
@@ -7,3 +8,6 @@
   (is (= (visit-leaf (fn [leaf] (assoc leaf :val (inc (:val leaf)))) t1)
          {:op :node, :left {:op :leaf, :val 2}, :right {:op :leaf, :val 3}})))
 
+(deftest force-visit-leaf-test
+  (is (= (w/prewalk identity (visit-leaf (fn [leaf] (assoc leaf :val (inc (:val leaf)))) t1))
+         {:op :node, :left {:op :leaf, :val 2}, :right {:op :leaf, :val 3}})))
